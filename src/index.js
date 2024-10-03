a500dc7d34b6bf919dfd74f045095ba2e8568a92
Yusuf Çoban <pyusuf24@gmail.com>
2024-10-03 23:51:50 +0300

    star rating

--- src/index.js
+++ src/index.js
@@ -1,13 +1,32 @@
 import React from "react";
+import { useState } from "react";
+
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import StarRating from "./StartRating";
 
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+
+  return (
+    <div>
+      <StarRating color="blue" maxRating={10} onSetRating={setMovieRating} />
+      <p>This movie was rated {movieRating} stars</p>
+    </div>
+  );
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/*<App />*/}
-    <StarRating maxRating={5} />
+    <StarRating
+      maxRating={5}
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+    />
+    <StarRating size={24} color="red" className="test" defaultRating={2} />
+
+    <Test />
   </React.StrictMode>
 );
